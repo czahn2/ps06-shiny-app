03fda014c1d27ca71c26dd6999d3861f20243aee
Camille Zahn <czahn2@uw.edu>
2023-03-02 14:46:11 -0800

    done!

--- README.md
+++ README.md
@@ -1,1 +1,16 @@
 # ps06-shiny-app
+
+For this PS I used the global temperature data provided. This data includes 
+the variables region, year, month, and temperature, and provides information
+about temperatures in different climates across the globe.
+
+The plot is showing temperature data over time. There is a checkbox group input
+to choose between seeing the entire globe, only land temperatures, only ocean
+temperatures, or any combination of the three. There is also a radio button
+input to select the plot color. This does not change the data, only the visuals.
+
+The table shows the data in an easy to understand way, and the slider input
+allows the user to select a range of years to see the data from. 
+
+link to shiny: https://czahn2.shinyapps.io/ps06-shiny-app/
+
--- app.R
+++ app.R
@@ -30,7 +30,11 @@ ui <- fluidPage(
               sidebarPanel(
                 checkboxGroupInput("regions", label = "Choose regions",
                                    choices = c("globe", "globe_land", "globe_ocean"),
-                                   selected = c("globe", "globe_land", "globe_ocean"))
+                                   selected = c("globe", "globe_land", "globe_ocean")),
+                radioButtons("color", "Select a color:",
+                           choices =   c("red","blue","pink"),
+                           selected = "red"),
+                textOutput("color_text"),
               ),
               mainPanel(plotOutput("plot"))
             )),     
@@ -41,7 +45,8 @@ ui <- fluidPage(
                 sliderInput("year_range", label = "Choose the year range",
                           min = min(data$year),
                           max = max(data$year),
-                          value = c(1978, 2023))
+                          value = c(1978, 2023)),
+                textOutput("year_text"),
             ),
             mainPanel(
               dataTableOutput("dataTable")
@@ -54,18 +59,31 @@ ui <- fluidPage(
 server <- function(input, output) {
 
   output$dataTable <- renderDataTable({
-    data
+    data %>%
+      filter(year >= input$year_range[1],
+             year <= input$year_range[2])
   })
   
+  
   output$plot <- renderPlot({
+    color <- switch(input$color, 
+                    red = "red",
+                    blue = "blue",
+                    pink = "pink")
     data %>%
       filter(region %in% input$regions) %>%
-      filter(year >= input$year_range[1],
-             year <= input$year_range[2]) %>%
       group_by(region) %>%
-      ggplot(aes(year, temp, group = region, color = factor(region))) +
-      geom_line() +
-      geom_point()
+      ggplot(aes(year, temp, group = region, col = factor(region))) +
+      ggtitle("Global Temperature Data")+
+      scale_fill_manual(values = color) +
+      geom_line(color = input$color) +
+      geom_point(color = input$color)
+  })
+  output$color_text <- renderText({
+    paste("You chose: ", input$color)
+  })
+  output$year_text <- renderText({
+    paste("You chose:", input$year_range[1],"-", input$year_range[2])
   })
 }
 
@@ -77,23 +95,5 @@ shinyApp(ui = ui, server = server)
 
 ## make year selector actually work..........................
 
-##Your github repo should include a readme file that contains a brief "user documentation" of
-##your project: a brief description of the data, and explanation what are the widgets and panels
-##doing. This file should contain a link to your project on the https://www.shinyapps.io
-##server.
+## publish to shiny, put link in readme
 
-##When you start the app, it should show explanatory text about your dataset and the app on
-##the main panel. It may be very brief. No separate sidebars and no interactivity is needed
-##here.
-##6. The explanatory text uses at least two html text formatting markers, such as strong(), em()
-##or similar (raw html is OK too).
-##See https://faculty.washington.edu/otoomet/info201-book/shiny.html#shiny-ui-text.
-
-##Both plot and table page must also include some textual output that reacts to a widget, such
-##as the number of non-missing observations, or the overall average value. For instance “Selected
-##subset contains 1234 observations” is such a sentence, given 1234 is derived from the actual
-##(subset of) data.
-
-## The plot also includes a widget that allows one to change the visuals only, leaving data
-##untouched.
-##See https://faculty.washington.edu/otoomet/info201-book/shiny.html#shiny-server-reactive
